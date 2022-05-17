cc5a9575352b723d1218278c7f5ca97bfb294d61
bft7658 <bft7658@gmail.com>
2022-05-17 14:29:33 +0800

    feat: add handlebars

--- app.js
+++ app.js
@@ -1,24 +1,25 @@
 const express = require('express')
 const mongoose = require('mongoose')
+const exphbs = require('express-handlebars')
 
 const app = express()
 const port = 3000
 
 mongoose.connect(process.env.MONGODB_URI, { useNewUrlParser: true, useUnifiedTopology: true })
-
 // 取得資料庫連線狀態
 const db = mongoose.connection
-// 連線異常
 db.on('error', () => {
   console.log('mongodb error!')
 })
-// 連線成功
 db.once('open', () => {
   console.log('mongodb connected!')
 })
 
+app.engine('handlebars', exphbs({ default: 'main'}))
+app.set('view engine', 'handlebars')
+
 app.get('/', (req, res) => {
-  res.send('hello world')
+  res.render('index')
 })
 
 app.listen(port, () => {
