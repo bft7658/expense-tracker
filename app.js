e964660f9765aa36d9dd7e030684bf562687803e
bft7658 <bft7658@gmail.com>
2022-05-18 17:46:52 +0800

    feat: add update record function

--- app.js
+++ app.js
@@ -28,6 +28,7 @@ app.get('/', (req, res) => {
   Record.find()
     .populate('categoryId')
     .lean()
+    .sort({ date: 'desc' })
     .then(records => {
       records.forEach(record => {
         record.date = dayjs(record.date).format('YYYY-MM-DD')
@@ -40,16 +41,49 @@ app.get('/', (req, res) => {
 app.get('/records/new', (req, res) => {
   Category.find()
     .lean()
+    .sort({ _id: 'asc' })
     .then(categories => {
       res.render('new', { categories })
     })
+    .catch(err => console.log(err))
 })
 
 app.post('/records', (req, res) => {
-  const { name, categoryId, date, amount } = req.body
   return Record.create(req.body)
     .then(() => res.redirect('/'))
-    .catch(error => console.log(error))
+    .catch(err => console.log(err))
+})
+
+app.get('/records/:id/edit', (req, res) => {
+  const id = req.params.id
+  return Record.findById(id)
+    .populate('categoryId')
+    .lean()
+    .then(record => {
+      record.date = dayjs(record.date).format('YYYY-MM-DD')
+      // 從 Category 拿出資料時，剔除掉 record 本身已經附加的類別，才不會出現兩個同樣的類別
+      Category.find({ _id: { $ne: record.categoryId }})
+      .lean()
+      .then(category => {
+        res.render('edit', { record, category })
+      })
+    })
+    .catch(err => console.log(err))
+})
+
+app.post('/records/:id/edit', (req, res) => {
+  const id = req.params.id
+  const { name, categoryId, date, amount } = req.body
+  return Record.findById(id)
+    .then(record => {
+      record.name = name
+      record.categoryId = categoryId
+      record.amount = amount
+      record.date = date
+      record.save()
+    })
+    .then(() => res.redirect('/'))
+    .catch(err => console.log(err))
 })
 
 app.listen(port, () => {
