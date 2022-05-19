fbe13ef1512f4574b7e8dee5fae802ae3348ad67
bft7658 <bft7658@gmail.com>
2022-05-19 10:29:12 +0800

    refactor: modify mongoose config

--- models/seeds/categorySeeder.js
+++ models/seeds/categorySeeder.js
@@ -1,12 +1,7 @@
-const mongoose = require('mongoose')
 const Category = require('../category')
 const categoryData = require('./categoryData.json').results
-mongoose.connect(process.env.MONGODB_URI, { useNewUrlParser: true, useUnifiedTopology: true })
+const db = require('../../config/mongoose')
 
-const db = mongoose.connection
-db.on('error', () => {
-  console.log('mongodb error!')
-})
 db.once('open', () => {
   Category.find()
     .then(categories => {
--- models/seeds/recordSeeder.js
+++ models/seeds/recordSeeder.js
@@ -1,13 +1,8 @@
-const mongoose = require('mongoose')
 const Record = require('../record')
 const recordData = require('./recordData.json').results
 const Category = require('../category')
-mongoose.connect(process.env.MONGODB_URI, { useNewUrlParser: true, useUnifiedTopology: true })
+const db = require('../../config/mongoose')
 
-const db = mongoose.connection
-db.on('error', () => {
-  console.log('mongodb error!')
-})
 db.once('open', async () => {
   Promise.all(Array.from(recordData, (record) => {
     return Category.findOne({ name: record.category })
