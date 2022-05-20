c0a2f1c9e34b990586f8bf34999d49097e9cb24e
bft7658 <bft7658@gmail.com>
2022-05-20 13:53:41 +0800

    feat: add bcrypt.js

--- routes/modules/users.js
+++ routes/modules/users.js
@@ -1,6 +1,7 @@
 const express = require('express')
 const router = express.Router()
 const passport = require('passport')
+const bcrypt = require('bcryptjs')
 
 const User = require('../../models/user')
 
@@ -24,7 +25,7 @@ router.post('/register', (req, res) => {
   if (!name || !email || !password || !confirmPassword) {
     errors.push({ message: '所有欄位都是必填。' })
   }
-  if (password !== !confirmPassword) {
+  if (password !== confirmPassword) {
     errors.push({ message: '密碼與確認密碼不相符！' })
   }
   if (errors.length) {
@@ -47,17 +48,18 @@ router.post('/register', (req, res) => {
           password,
           confirmPassword
         })
-      } else {
-        return User.create({
+      } 
+      return bcrypt
+        .genSalt(10)
+        .then(salt => bcrypt.hash(password, salt))
+        .then(hash => User.create({
           name,
           email,
-          password
-        })
-          .then(() => res.redirect('/'))
-          .catch(err => console.log(err))
-      }
+          password: hash
+        }))
+        .then(() => res.redirect('/'))
+        .catch(err => console.log(err))
     })
-    .catch(err => console.log(err))
 })
 
 router.get('/logout', (req, res) => {
