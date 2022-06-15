a4ece161c6d6b679806e20e563a8867fa16191f6
bft7658 <bft7658@gmail.com>
2022-06-15 11:32:07 +0800

    feat: modify record model & add date filter function

--- routes/modules/home.js
+++ routes/modules/home.js
@@ -5,25 +5,21 @@ const Record = require('../../models/record')
 const Category = require('../../models/category')
 const dayjs = require('dayjs')
 
-router.get('/', (req, res) => {
+router.get('/', async (req, res) => {
   const userId = req.user._id
-  Category.find()
-    .lean()
-    .then(categories => {
-      return Record.find({ userId })
-        .populate('categoryId')
-        .lean()
-        .sort({ date: 'desc' })
-        .then(records => {
-          let totalAmount = 0
-          records.forEach(record => {
-            totalAmount += record.amount
-            record.date = dayjs(record.date).format('YYYY-MM-DD')
-          })
-          res.render('index', { records, categories, totalAmount })
-        })
-        .catch(err => console.error(err))
+  try {
+    const categories = await Category.find().sort({ _id: 'asc' }).lean()
+    const records = await Record.find({ userId }).populate('categoryId').sort({ date: 'desc' }).lean()
+
+    let totalAmount = 0
+    records.forEach(record => {
+      totalAmount += record.amount
+      record.date = dayjs(record.date).format('YYYY-MM-DD')
     })
+    res.render('index', { records, categories, totalAmount })
+  } catch (error) {
+    return console.log(error)
+  }
 })
 
 // 篩選資料
@@ -31,13 +27,15 @@ router.get('/filter', async (req, res) => {
   const userId = req.user._id
   const sort = req.query.sort
   const categorySelected = req.query.category || ''
+  const monthSelected = req.query.month
   try {
-    const categories = await Category.find().lean()
-    const searchKey = {
-      userId
+    const categories = await Category.find().sort({ _id: 'asc' }).lean()
+    const filterQuery = {
+      userId,
+      date: { $regex: monthSelected }
     }
-    if (categorySelected) searchKey.categoryId = categorySelected
-    const records = await Record.find(searchKey)
+    if (categorySelected) filterQuery.categoryId = categorySelected
+    const records = await Record.find(filterQuery)
       .populate('categoryId')
       .sort(JSON.parse(sort))
       .lean()
@@ -47,7 +45,7 @@ router.get('/filter', async (req, res) => {
       totalAmount += record.amount
       record.date = dayjs(record.date).format('YYYY-MM-DD')
     })
-    res.render('index', { records, sort, categories, totalAmount, categorySelected })
+    res.render('index', { records, sort, categories, totalAmount, categorySelected, monthSelected })
   } catch (error) {
     return console.log(error)
   }
