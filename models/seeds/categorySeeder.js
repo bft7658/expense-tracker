08c964816b88dbb18ef785ea2d57235fe44015f1
bft7658 <bft7658@gmail.com>
2022-06-19 14:42:12 +0800

    feat: modify seed with async await

--- models/seeds/categorySeeder.js
+++ models/seeds/categorySeeder.js
@@ -2,20 +2,9 @@ const Category = require('../category')
 const categoryData = require('./categoryData.json').results
 const db = require('../../config/mongoose')
 
-db.once('open', () => {
-  Category.find()
-    .then(categories => {
-      if (categories.length) {
-        console.log('已載入過"消費類別"了')
-        process.exit()
-      }
-      return Promise.all(Array.from(categoryData, item => {
-        return Category.create({ name: item.name, icon: item.icon })
-      }))
-    })
-    .then(() => {
-      console.log('"消費類別"的種子資料載入完畢')
-      process.exit()
-    })
-    .catch(err => console.log(err))
+db.once('open', async () => {
+  await Category.create(categoryData)
+  
+  console.log('"消費類別"的種子資料載入完畢')
+  process.exit()
 })
--- models/seeds/recordSeeder.js
+++ models/seeds/recordSeeder.js
@@ -6,64 +6,22 @@ const Record = require('../record')
 const recordData = require('./recordData.json').results
 const Category = require('../category')
 const User = require('../user')
-const userData = require('./userData.json').results
+const userData = require('./userData.json').results[0]
 const db = require('../../config/mongoose')
 
-// 參考別人的方法
 db.once('open', async () => {
-  // 先將 record 與 category 做關聯
-  const categoryData = await Category.find().select('name').lean()
-  recordData.forEach(record => {
-    record.categoryId = categoryData.find(category => record.category === category.name)._id
-  })
-
-  // 再把 record 與 user 做關聯
-  Promise.all(Array.from(userData, async (user) => {
-    const { name, email, password } = user
-    const hash = await bcrypt.genSalt(10).then(salt => bcrypt.hash(password, salt))
-    const userId = await User.create({ name, email, password: hash }).then(user => user._id)
+    // 先將 record 與 category 做關聯
+    const categoryData = await Category.find().select('name').lean()
+    recordData.forEach(record => {
+      record.categoryId = categoryData.find(category => record.category === category.name)._id
+    })
+    // 再把 record 與 user 做關聯
+    const hash = await bcrypt.hash(userData.password, 10)
+    const user = await User.create({ name: userData.name, email: userData.email, password: hash })
+    const userId = user._id
     recordData.forEach(record => record.userId = userId)
     await Record.create(recordData)
-  }))
-    .then(() => {
-      console.log('"消費紀錄"的種子資料載入完畢')
-      process.exit()
-    })
-})
 
-// 自己原本的作法，還沒優化出來
-// db.once('open', () => {
-//   // 先讓 record 跟 category 做關聯
-//   Promise.all(Array.from(recordData, record => {
-//     return Category.findOne({ name: record.category })
-//       .lean()
-//       .then(category => {
-//         record.categoryId = category._id
-//         return Record.create(record)
-//       })
-//   }))
-//   return Promise.all(Array.from(userData, seedUser => {
-//     const { name, email, password } = seedUser
-//     return bcrypt.genSalt(10)
-//       .then(salt => bcrypt.hash(password, salt))
-//       .then(hash => {
-//         return User.create({
-//           name,
-//           email,
-//           password: hash
-//         })
-//           .then(user => {
-//             const userId = user._id
-//             Promise.all(Array.from(recordData, record => {
-//               record.userId = userId
-//               return Record.create(record)
-//             }))
-//           })
-//       })
-//   }))
-//     .then(() => {
-//       console.log('"消費紀錄"的種子資料載入完畢')
-//       process.exit()
-//     })
-//     .catch(err => console.log(err))
-// })
+    console.log('"消費紀錄"的種子資料載入完畢')
+    process.exit()
+})
